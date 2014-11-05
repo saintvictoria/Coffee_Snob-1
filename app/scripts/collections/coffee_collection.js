db5b75c4e4a3fb1eeb1565f7c7bf403a0a43857c
Tim Whitacre <tim@timw.co>
2014-11-05 09:49:27 -0500

    Fixed errors

--- app/scripts/collections/coffee_collection.js
+++ app/scripts/collections/coffee_collection.js
@@ -1,7 +1,7 @@
 (function () {
 
   App.Collections.Coffees = Backbone.Collection.extend({
-    model: App.Model.Coffee,
+    model: App.Models.Coffee,
     url: 'https://tiy-atl-fe-server.herokuapp.com/collections/coffee'
   });
 
