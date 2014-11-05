a39dad24edc6194a705bb9629f02cd52bdc92613
Tim Whitacre <tim@timw.co>
2014-11-05 09:59:38 -0500

    Added first view and template script

---
+++ app/scripts/views/add_coffee_view.js
@@ -0,0 +1,17 @@
+(function () {
+
+  App.Views.AddCoffee = Backbone.View.extend({
+
+    el: '#coffeeForm',
+
+    initialize: function () {
+      this.render();
+    },
+
+    render: function () {
+      
+    }
+
+  });
+
+}());
