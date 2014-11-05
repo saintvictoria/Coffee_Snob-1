2d772dfdd08b1f9234fb6137ec0bac2cb566f5b8
Tim Whitacre <tim@timw.co>
2014-11-05 11:37:43 -0500

    Added delete and updating

--- app/scripts/views/single_coffee_view.js
+++ app/scripts/views/single_coffee_view.js
@@ -5,13 +5,18 @@
     tagName: 'ul',
     className: 'coffeeSingle',
 
-    events: {},
+    events: {
+      'submit #updateCoffee' : 'updateCoffee',
+      'click #delete' : 'deleteCoffee'
+    },
 
     template: _.template($('#singleTemp').html()),
 
     initialize: function (options) {
       this.options = options;
       this.render();
+
+      $('#coffeeForm').empty();
 
       // Get our Element On Our Page
       $('#coffeeList').html(this.$el);
@@ -22,6 +27,35 @@
       this.$el.empty();
 
       this.$el.html(this.template(this.options.coffee.toJSON()));
+
+    },
+
+    updateCoffee: function (e) {
+      e.preventDefault();
+
+      // Update our Model Instance
+      this.options.coffee.set({
+        name: $('#update_name').val(),
+        brand: $('#update_brand').val(),
+        comments: $('#update_comments').val()
+      });
+
+      // Save Instance
+      this.options.coffee.save();
+
+      // Go back to our home page
+      App.router.navigate('', {trigger: true});
+
+    },
+
+    deleteCoffee: function (e) {
+      e.preventDefault();
+
+      // Remove Coffee
+      this.options.coffee.destroy();
+
+      // Go home ET
+      App.router.navigate('', {trigger: true});
 
     }
 
