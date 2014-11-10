4b0c37f098de4bbee20baa0d8b939fde8b348b59
Victoria Nicholson <saintvictoria@gmail.com>
2014-11-10 09:34:51 -0500

    chnages

--- app/scripts/views/list_coffee_view.js
+++ app/scripts/views/list_coffee_view.js
@@ -10,7 +10,9 @@
     template: _.template($('#listTemp').html()),
 
     initialize: function (options) {
+
       this.options = options;
+
       this.render();
 
       this.collection.off();
@@ -19,6 +21,8 @@
       // Get our Element On Our Page
       $('#coffeeList').html(this.$el);
 
+
+
     },
 
     render: function () {
@@ -26,23 +30,30 @@
 
       // Empty out
       this.$el.empty();
-      this.collection.sort();
 
-      if(this.options.sort !== undefined) {
+      // Sorting On The Fly
+      if (this.options.sort != undefined) {
+        // Setting up a localized collection to sort by our sort param
+        var local_collection = this.collection.sortBy( function (model) {
+          return model.get(self.options.sort);
+        });
+        _.each(local_collection, function (c) {
+          self.$el.append(self.template(c.toJSON()));
+        })
+      } else {
+        // Sort from our default comparator in our collection constructor
+        this.collection.sort();
+        this.collection.each(function (c) {
+          self.$el.append(self.template(c.toJSON()));
+        });
+      }
 
-      var local_collection = this.collection.sortBy( function(model){
-        return model.get(self.options.sort);
-      });
-      _.each(local_collection, function (c) {
-        self.$el.append(self.template(c.toJSON()));
-      })
-    } else {
-      this.collection.sort();
-      this.collection.each(function (c) {
-        self.$el.append(self.template(c.toJSON()));
-      });
-    }
 
+      if (this.options.showTwitter) {
+        $('.hero-unit h1 a').html('Twitter');
+      } else {
+        $('.hero-unit h1 a').html('Coffee Snob');
+      }
       return this;
     }
 
